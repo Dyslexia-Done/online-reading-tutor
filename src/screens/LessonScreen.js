9c16b075b5b5e91e75e6f6d700c67140cea24cab
Yusra Syeda <yusra.syeda@ibm.com>
2020-04-27 22:59:41 -0400

    read question and answers from array, and make them clickable

--- src/screens/LessonScreen.js
+++ src/screens/LessonScreen.js
@@ -1,11 +1,26 @@
 import * as React from "react";
 import Constants from "../components/Constants.js";
-import { Text, View, Button, Alert } from "react-native";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
 import { alertBadgeAcquired } from "../components/BadgeHelpers";
+
+
+SampleFunction=(item)=>{
+
+  Alert.alert(item);
+}
+
 function LessonScreen(props) {
+
+  var array = ["dobe", "dobe", "dode", "doke"];
+
   return (
     <View>
       <Text>Lesson Simulation</Text>
+
+       { array.map((item, key)=>(
+         <Text key={key} style={styles.TextStyle} onPress={ SampleFunction.bind(this, item) }> { item } </Text>)
+      )}
+
       <Button
         title="Click here to finish lesson (go back to home screen)"
         onPress={() => {
@@ -20,5 +35,19 @@ function LessonScreen(props) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+
+ MainContainer: {
+   flex: 1,
+   margin: 10
+ },
+
+ TextStyle:{
+   fontSize : 25,
+    textAlign: 'center'
+ }
+ 
+});
 
 export default LessonScreen;
