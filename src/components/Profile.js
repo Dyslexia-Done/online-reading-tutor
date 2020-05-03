96f49d4ed147a3e7baa0d93cd48c80b1dade9be2
Asma Hassan <asma.hassan@ibm.com>
2020-05-02 21:51:44 -0400

    Add static picture to the profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Text, View, SafeAreaView, StyleSheet, ScrollView, Image, Button} from "react-native";
+import { Text, View, SafeAreaView, StyleSheet, ScrollView, Image, Button, ImageBackground } from "react-native";
 
 // local components
 import { StartButton } from "./index";
@@ -10,49 +10,60 @@ import {
 } from "../components/Helpers.js";
 
 const Profile = (props) => {
-        return (
-          <SafeAreaView style={styles.container}>
-              <View style={styles.topContainer}>
-                    <View style={styles.imageContainer}>
-                        <View style={styles.editbutton}>
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.topContainer}>
+                <View style={styles.imageContainer}>
+                <View style={styles.profileContainer}>
+                        <ImageBackground
+                            style={styles.profilePic}
+                            source={require('../assets/avatar/body.png')}>
+                            <Image
+                                style={styles.profileBodyParts}
+                                source={require('../assets/avatar/mid/cyclops.png')} />
+                            <Image
+                                style={styles.bodyPart}
+                                source={require('../assets/avatar/bottom/toothy.png')} />
+                        </ImageBackground>
+                    </View>
+                    <View style={styles.editbutton}>
                         <Button title="" onPress={() => this.editAvatar()} />
-                        </View>
-                        
                     </View>
-                    <Text style={styles.nameText}> John Doe </Text>
-                    <View style={styles.streakdisplay}>
-                        <View style={styles.streakContainer}>
+                </View>
+                <Text style={styles.nameText}> John Doe </Text>
+                <View style={styles.streakdisplay}>
+                    <View style={styles.streakContainer}>
 
-                        </View>
-                       <View style={styles.streakContainer}>
-
-                        </View>
                     </View>
-              </View>
-              <View style={styles.badgeContainer}>
-              <Text style={styles.badgeText}> Badges Earned </Text>
-                 <ScrollView horizontal={true} style={styles.badgeContainer}>
+                    <View style={styles.streakContainer}>
+
+                    </View>
+                </View>
+            </View>
+            <View style={styles.badgeContainer}>
+                <Text style={styles.badgeText}> Badges Earned </Text>
+                <ScrollView horizontal={true} style={styles.badgeContainer}>
                     <Image
-                    source={require("../assets/badges/world_completion.png")}
-                    style={styles.ImageIconStyle}
+                        source={require("../assets/badges/world_completion.png")}
+                        style={styles.ImageIconStyle}
                     />
                     <Image
-                    source={require("../assets/badges/world_completion.png")}
-                    style={styles.ImageIconStyle}
+                        source={require("../assets/badges/world_completion.png")}
+                        style={styles.ImageIconStyle}
                     />
                     <Image
-                    source={require("../assets/badges/world_completion.png")}
-                    style={styles.ImageIconStyle}
-                    />                    
+                        source={require("../assets/badges/world_completion.png")}
+                        style={styles.ImageIconStyle}
+                    />
                     <Image
-                    source={require("../assets/badges/world_completion.png")}
-                    style={styles.ImageIconStyle}
-                    />                   
-                 </ScrollView>
-              </View>
+                        source={require("../assets/badges/world_completion.png")}
+                        style={styles.ImageIconStyle}
+                    />
+                </ScrollView>
+            </View>
             <StartButton text="Start" onPress={() => this.goToFire()} />
-          </SafeAreaView>
-        );
+        </SafeAreaView>
+    );
 }
 
 export default Profile;
@@ -60,8 +71,8 @@ export default Profile;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, 
-        backgroundColor: "black", 
+        flex: 1,
+        backgroundColor: "black",
         paddingBottom: 100,
     },
 
@@ -81,36 +92,54 @@ const styles = StyleSheet.create({
         backgroundColor: variables.palette.blue.primary,
         marginTop: 30,
         marginBottom: 10,
-        shadowOffset: { width: 0, height: 4},
+        shadowOffset: { width: 0, height: 4 },
         shadowOpacity: 0.5,
         shadowRadius: 5,
         elevation: 5,
         flexDirection: "row-reverse",
+
     },
 
-
+    profileContainer: {
+        overflow: "hidden",
+        width: 180,
+        height: 180,
+        borderRadius: 100,
+        alignSelf: "center",
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     profilePic: {
-        width: 70,
-        height: 100,
+        width: 90,
+        height: 156,
+        marginTop: 60,
+        resizeMode: 'contain',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 
-    profileBodyParts:{
+    profileBodyParts: {
+        marginTop: 15,
         width: 70,
         height: 50,
+        resizeMode: 'contain'
     },
 
     bodyPart: {
-        width: 70,
-        height: 100,
+        flex: 1,
+        width: 50,
+        height: 50,
+        resizeMode: 'contain'
     },
 
-    editbutton:{
+    editbutton: {
         width: 50,
         height: 50,
         borderRadius: 100,
         backgroundColor: variables.palette.pink.primary,
         shadowColor: 'black',
-        shadowOffset: { width: 0, height: 4},
+        shadowOffset: { width: 0, height: 4 },
         shadowOpacity: 0.5,
         shadowRadius: 5,
         elevation: 5,
@@ -130,7 +159,7 @@ const styles = StyleSheet.create({
 
     streakdisplay: {
         flexDirection: "row",
-        justifyContent:'space-between',
+        justifyContent: 'space-between',
         marginTop: 45
     },
     streakContainer: {
@@ -152,7 +181,7 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         margin: 10,
     },
-    backgroundImage:{
+    backgroundImage: {
         flex: 1,
         width: '100%',
         height: '100%',
